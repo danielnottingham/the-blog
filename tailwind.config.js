df371ff0a69d5fd3d4ef81aa72f6e540ebf77e39
Daniel Nottingham <danielnottingham.ti@gmail.com>
2022-10-10 18:04:00 -0300

    config: flowbite and generate a home static page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  safelist: ['hidden'],
+  plugins: [require('flowbite/plugin')],
+  theme: {
+    fontFamily: {
+      sans: ['Inter var', 'sans-serif']
+    }
+  }
 }
